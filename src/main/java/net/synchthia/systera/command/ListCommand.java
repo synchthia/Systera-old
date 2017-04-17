b2438638344a932a01087e8a99c22a9463ef2832
DESKTOP-5OVEU9N\Laica-Lunasys <laica.lunasys@gmail.com>
2017-04-18 01:26:29 +0900

    [Add] Added List command

--- src/main/java/net/synchthia/systera/command/ListCommand.java
+++ src/main/java/net/synchthia/systera/command/ListCommand.java
@@ -1,7 +1,48 @@
 package net.synchthia.systera.command;
 
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissions;
+import net.synchthia.systera.SysteraPlugin;
+import net.synchthia.systera.permissions.PermissionsAPI;
+import net.synchthia.systera.player.VanishManager;
+import net.synchthia.systera.util.StringUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 /**
  * @author Laica-Lunasys
  */
 public class ListCommand {
+    @Command(aliases = {"list", "who"}, desc = "List online players")
+    @CommandPermissions("systera.command.list")
+    public static void list(final CommandContext args, CommandSender sender, SysteraPlugin plugin) throws CommandException {
+        StringBuilder messages = new StringBuilder();
+        Integer count = 0;
+
+        if (sender.hasPermission("systera.command.vanish")) {
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                String nameString = PermissionsAPI.getPrefix(onlinePlayer) + onlinePlayer.getName();
+                if (VanishManager.getVanishPlayerInServer().contains(onlinePlayer)) {
+                    messages.append(StringUtil.coloring("&e&l[Vanish]" + nameString));
+                } else {
+                    messages.append(StringUtil.coloring(nameString));
+                }
+                if (count < Bukkit.getOnlinePlayers().size() - 1) messages.append(ChatColor.WHITE + ", ");
+                count = count + 1;
+            }
+        } else {
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                messages.append(StringUtil.coloring(PermissionsAPI.getPrefix(onlinePlayer) + onlinePlayer.getName()));
+                if (count < Bukkit.getOnlinePlayers().size() - 1) messages.append(ChatColor.WHITE + ", ");
+                count = count + 1;
+            }
+        }
+
+        sender.sendMessage(StringUtil.coloring("&bOnline &6(" + count + "/" + Bukkit.getMaxPlayers() + ")"));
+        sender.sendMessage(messages.toString());
+    }
 }
